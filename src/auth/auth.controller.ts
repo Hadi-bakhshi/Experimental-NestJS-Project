a5cc591531e9aff74dbbc037b0ef01d044052e3d
Hadi-bakhshi <hadibakhshi277@gmail.com>
2023-02-18 15:28:42 +0330

    feat: add DTO
    - add dto which stands for data transfer object, declare type for it

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import type { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signUp() {
+  signUp(@Body() dto: AuthDto) {
+    console.log({ dto });
     return this.authService.signUp();
   }
 
