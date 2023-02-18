22abeb71d9633144835b071a12a08c2548486cd3
Hadi-bakhshi <hadibakhshi277@gmail.com>
2023-02-18 16:27:39 +0330

    feat: add new logic for signup
    - add new logic to hash the password and create the user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
+
+  async signUp(dto: AuthDto) {
+    // Generate The hashed Password
+    const hash = await argon.hash(dto.password);
+    // Save the use in the DB
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+      select: {
+        id: true,
+        email: true,
+        firstName: true,
+        lastName: true,
+      },
+    });
+    return user;
+  }
   signIn() {
     return { msg: 'Hey, You are signed in' };
-  }
-
-  signUp() {
-    return { msg: 'Hey, You are signed up' };
   }
 }
