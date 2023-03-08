8d3b4b207920c1b3e52c60e6095c47b304161860
Hadi-bakhshi <hadibakhshi277@gmail.com>
2023-03-08 17:13:04 +0330

    feat: signup is almost done

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { Prisma } from '@prisma/client';
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
@@ -9,20 +10,30 @@ export class AuthService {
   async signUp(dto: AuthDto) {
     // Generate The hashed Password
     const hash = await argon.hash(dto.password);
-    // Save the use in the DB
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-      select: {
-        id: true,
-        email: true,
-        firstName: true,
-        lastName: true,
-      },
-    });
-    return user;
+    try {
+      // Save the use in the DB
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+        select: {
+          id: true,
+          email: true,
+          firstName: true,
+          lastName: true,
+        },
+      });
+      // delete user.hash;
+      return user;
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('credentials taken');
+        }
+      }
+      throw error;
+    }
   }
   signIn() {
     return { msg: 'Hey, You are signed in' };
