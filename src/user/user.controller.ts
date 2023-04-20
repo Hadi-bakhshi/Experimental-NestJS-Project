213f2af731b6f0ae35ad6226226db82b2d841591
Hadi-bakhshi <hadibakhshi277@gmail.com>
2023-04-20 12:29:19 +0330

    feat: add new protected route add new route which is protected and will check who you are

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('user')
-export class UserController {}
+import { Controller, Get, UseGuards, Req } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+@Controller('users')
+export class UserController {
+  @UseGuards(AuthGuard('jwt'))
+  @Get('me')
+  getMe(@Req() req: Request) {
+    return req.user;
+  }
+}
